7263a7cad18308c36b05d8c3f17c8cbc91c0f344
jiwagon <jiwagon11@gmail.com>
2023-10-25 02:57:31 -0400

    final commit

--- ios101-project6-tumblr/DetailViewController.swift
+++ ios101-project6-tumblr/DetailViewController.swift
@@ -6,24 +6,43 @@
 //
 
 import UIKit
+import Nuke
 
 class DetailViewController: UIViewController {
-
+    
+    var post: Post!
+    
+    @IBOutlet weak var postImageView: UIImageView!
+    
+    @IBOutlet weak var captionTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+            
+        if let caption = post.caption.trimHTMLTags() {
+            captionTextView.text = "\(caption)"
+        } else {
+            captionTextView.text = ""
+        }
+        
+        // MARK: - Configure the image views
 
-        // Do any additional setup after loading the view.
+        // Unwrap the optional poster path
+        if let photo = post.photos.first {
+           let url = photo.originalSize.url
+        Nuke.loadImage(with: url, into: postImageView)
+        }
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
